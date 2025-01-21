ca1c2acac2314a197a172906289068807330820f
alan890104 <alan890104@gmail.com>
2025-01-21 20:51:31 +0800

    refactor(types): remove unused simulation types to streamline codebase
    - Deleted commented-out type definitions for deposit, swap, and withdraw simulation parameters and results in src/types.ts.
    - This cleanup enhances code readability and maintainability by removing obsolete code.

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,5 @@
 import { Allocation } from '@torch-finance/core';
 
-// export type SimulateDepositParams = {
-//   depositAmounts: Allocation[]; // amount of tokens to be deposited
-//   rates?: Allocation[]; // external rates for yield bearing stable pool
-// };
-
-// export type SimulatorDepositResult = {
-//   lpTokenOut: bigint; // amount of lp tokens minted
-//   virtualPriceBefore: bigint; // virtual price before deposit
-//   virtualPriceAfter: bigint; // virtual price after deposit
-//   lpTotalSupply: bigint; // total supply of lp tokens
-// };
-
-// export type SimulateSwapParams = {
-//   assetIn: Asset; // asset to be swapped in
-//   assetOut: Asset; // asset to be swapped out
-//   amount: bigint; // amount of asset to be swapped
-//   rates?: Allocation[]; // external rates for yield bearing stable pool
-// };
-
-// export type SimulatorSwapResult = {
-//   amountOut: bigint; // amount of asset received
-//   virtualPriceBefore: bigint; // virtual price before swap
-//   virtualPriceAfter: bigint; // virtual price after swap
-// };
-
-// export type SimulateWithdrawParams = {
-//   lpAmount: bigint; // amount of lp tokens to be burned
-//   assetOut?: Asset; // asset to be withdrawn
-//   rates?: Allocation[]; // external rates for yield bearing stable pool
-// };
-
-// export type SimulateWithdrawResult = {
-//   amountOuts: bigint[]; // amount of assets received
-//   virtualPriceBefore: bigint; // virtual price before withdraw
-//   virtualPriceAfter: bigint; // virtual price after withdraw
-// };
-
 export interface SimulatorState {
   initA: number; // initial amplification coefficient
   futureA: number; // future amplification coefficient
